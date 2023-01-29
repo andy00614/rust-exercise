62e7e92d5c6304f27885cb27a95459512a5c5805
andy00614 <33732843@qq.com>
2023-01-29 11:28:00 +0800

    feat: use mod

---
+++ src/compond_type.rs
@@ -0,0 +1,5 @@
+pub fn array_type() {
+  let mut a: [i8; 10] = [42; 10];
+  a[5] = 0;
+  println!("a: {:?}", a);
+}
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+mod compond_type;
+fn base() {
+    // let mut x: i32 = 256;
+    let mut x = 256i16;
+    print!("{x}");
+    while x != 1 {
+        if x % 2 == 0 {
+            x = x / 2;
+        } else {
+            x = x * 3 + 1
+        }
+        print!("-> {x}")
+    }
+    println!()
+}
+fn main() {
+   base();
+   compond_type::array_type();
+}
